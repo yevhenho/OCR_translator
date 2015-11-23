1959654fc150299b0cdf79a6c27d8c5e2be32e90
yevhenho <y.semanyuk@gmail.com>
2015-11-23 23:54:46 +0200

    first

---
+++ WebSocketTest/app/src/main/java/com/example/yevhenho/websockettest/ReceiveActivity.java
@@ -0,0 +1,11 @@
+package com.example.yevhenho.websockettest;
+
+/**
+ * Created by yevhenho on 11/23/2015.
+ */
+public class ReceiveActivity extends BaseActivity {
+    @Override
+    void setActivityContentView() {
+    setContentView(R.layout.activity_receive);
+    }
+}
---
+++ WebSocketTest/app/src/main/java/com/example/yevhenho/websockettest/SendActivity.java
@@ -0,0 +1,18 @@
+package com.example.yevhenho.websockettest;
+
+import android.os.Bundle;
+
+/**
+ * Created by yevhenho on 11/23/2015.
+ */
+public class SendActivity  extends BaseActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    void setActivityContentView() {
+        setContentView(R.layout.activity_send);
+    }
+}
